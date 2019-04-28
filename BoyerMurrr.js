1ba31f4e6a57a5efc494fcf2cd2a90141f5285cd
Plugway <48772861+Plugway@users.noreply.github.com>
2019-04-28 05:35:01 +0500

    Triple purrrfect (^=◕ᴥ◕=^)

--- BoyerMurrr.js
+++ BoyerMurrr.js
@@ -10,10 +10,54 @@ function badCharHeuristic(strToFind, strToFindSize, badChars)
         badChars[strToFind.charCodeAt(i)] = i;
 }
 
+function preprocessSuffixHeuristic(shift, bpos, strToFind, m)
+{
+    var i = m;
+    var j = m + 1;
+
+    bpos[i] = j;
+
+    while (i > 0)
+    {
+        while (j <= m && strToFind[i - 1] != strToFind[j - 1])
+        {
+            if (shift[j] == 0)
+                shift[j] = j - i;
+            j = bpos[j];
+        }
+
+        i--;
+        j--;
+        bpos[i] = j;
+    }
+}
+
+function preprocessCase2(shift, bpos, strToFind, m)
+{
+    var i, j;
+    j = bpos[0];
+    for(i = 0; i <= m; i++)
+    {
+        if(shift[i] == 0)
+            shift[i] = j;
+        if (i == j)
+            j = bpos[j];
+    }
+}
+
 function search(text, strToFind)
 {
     var m = strToFind.length;
     var n = text.length;
+
+    var bpos = [m + 1];
+    var shift = [m + 1];
+
+    for (var i = 0; i < m + 1; i++)
+        shift[i] = 0;
+
+    preprocessSuffixHeuristic(shift, bpos, strToFind, m);
+    preprocessCase2(shift, bpos, strToFind, m);
 
     var badChars = [CMAX];
 
@@ -27,18 +71,16 @@ function search(text, strToFind)
         {
             //console.log("Found at " + s);
             outArr.push(s);
-            if (s+m < n)
-                s += m - badChars[text.charCodeAt(s+m)];
-            else
-                s += 1;
+
+            s += Math.max ((s+m < n)? m - badChars[text.charCodeAt(s+m)] : 1, shift[0]);
         }
         else
-            s += Math.max(1, j - badChars[text.charCodeAt(s+j)]);
+            s += Math.max(Math.max(1, j - badChars[text.charCodeAt(s+j)]), shift[j + 1]);
     }
 }
 
 var outArr = [];
 var text = 'abcccsba';
-var strToFind = 'sb';
+var strToFind = 'a';
 search(text, strToFind);
 console.log(outArr);
